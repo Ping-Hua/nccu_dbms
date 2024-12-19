133be9fa73708aa169edefc4e10e6571f9618b23
ycyueh <yichen900103@gmail.com>
2024-12-19 22:00:43 +0800

    fix:add columns

--- src/app/models/post_model.py
+++ src/app/models/post_model.py
@@ -7,5 +7,7 @@ class BookPost(Base):
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    book_condition = db.Column(db.Text, nullable=True)
     price = db.Column(db.Integer, nullable=False)
+    post_time = db.Column(db.DATETIME , nullable=False)
+    seller_id = db.Column(db.INTEGER, nullable=False)
