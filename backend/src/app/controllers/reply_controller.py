7102d8199029caf4867485014ee0f32651102084
shiloh <shilohlee98@gmail.com>
2024-12-23 23:50:56 +0800

    Finish the main function of ReplyController - fix the mismatch of columns name - finish get history function

--- backend/src/app/controllers/reply_controller.py
+++ backend/src/app/controllers/reply_controller.py
@@ -8,22 +8,22 @@ class ReplyController:
         logging.info("----Reply_controller.add_reply----")
 
         data = request.get_json()
+        from_user_id = data.get('from_user_id') # 回應者的 user 的 userId
+        to_user_id = data.get('to_user_id') # 貼文 user 的 userId
         post_id = data.get('post_id') # 貼文的 ID
-        to_id = data.get('to_id') # 貼文 user 的 userId
-        from_id = data.get('from_id') # 回應者的 user 的 userId
-        content = data.get('content') # 回應的文字
+        message = data.get('message') # 回應的文字
 
-        if not all([post_id, to_id, from_id, content]):
-            return jsonify({"error": "Missing required fields: post_id, to_id, from_id, content"}), 400
+        if not all([from_user_id, to_user_id, post_id, message]):
+            return jsonify({"error": "Missing required fields: from_user_id, to_user_id, post_id, message"}), 400
         
         try:
-            reply = ReplyService.add_reply(post_id, from_id, to_id, content)
+            reply = ReplyService.add_reply(from_user_id, to_user_id, post_id, message)
             return jsonify({
                 'reply_id': reply['reply_id'],
-                'from_id': reply['from_id'],
-                'to_id': reply['to_id'],
+                'from_user_id': reply['from_user_id'],
+                'to_user_id': reply['to_user_id'],
                 'post_id': reply['post_id'],
-                'content': reply['content'],
+                'message': reply['message'],
                 'reply_time': reply['reply_time']            
             }), 201
         
@@ -45,8 +45,21 @@ class ReplyController:
     
     def history(self):
         logging.info("----Reply_controller.history----")
-
-        # TODO: 實現留言歷史紀錄
-        return jsonify({"message": "Reply history retrieved successfully", "replies": []})
+        data = request.get_json()
+        seller_user_id = data.get('seller_user_id') # 貼文 user 的 userId
+        buyer_user_id = data.get('buyer_user_id') # 回應者的 user 的 userId
+        post_id = data.get('post_id') # 貼文的 ID
+        
+        if not all([seller_user_id, buyer_user_id, post_id]):
+            return jsonify({"error": "Missing required fields: from_user_id, to_user_id, post_id, message"}), 400
+        
+        try:
+            reply_history = ReplyService.get_reply_history(seller_user_id, buyer_user_id, post_id)
+            print(reply_history)
+            return jsonify({ "reply_history" : reply_history }), 201
+        
+        except Exception as e:
+            logging.error(f"Error getting reply history: {str(e)}")
+            return jsonify({"error": "An unexpected error occurred. Please try again later."}), 500
 
 reply_controller = ReplyController()
