24e8b4e7a4ce2d2a57aaba9b5464b014d0b44980
ycyueh <yichen900103@gmail.com>
2024-12-25 10:28:08 +0800

    add post

--- src/app/routes/post_routes.py
+++ src/app/routes/post_routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint
-from app.controllers.post_controller import PostController
+from app.controllers.post_controller import post_controller
 
 post_bp = Blueprint('post', __name__)
 
-# 建立控制器實例
-post_controller = PostController()
+
 
 post_bp.add_url_rule('/add_post', view_func=post_controller.add_post, methods=['POST'])
 post_bp.add_url_rule('/update_post', view_func=post_controller.update_post, methods=['PUT'])
--- src/app/schema.sql
+++ src/app/schema.sql
@@ -1,3 +1,4 @@
+'''
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,4 +17,66 @@ CREATE TABLE book_posts (
     -- 外鍵約束
    -- FOREIGN KEY (seller_id) REFERENCES sellers (id),
    -- FOREIGN KEY (isbn) REFERENCES books (isbn)
+);
+'''
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS reply;
+DROP TABLE IF EXISTS review;
+
+CREATE TABLE user (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    student_number INTEGER, -- not null
+    phone INTEGER, -- not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE post (
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    seller_user_id INTEGER, -- fk
+    book_id INTEGER, -- fk
+    book_condition TEXT, -- not null
+    price INTEGER, -- not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE book (
+    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ISBN INTEGER, -- pk?
+    book_name TEXT, -- not null
+    author TEXT, --not null
+    version TEXT, -- not null
+    public_year YEAR, -- not null
+    publisher TEXT, --not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE genre (
+    genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    genre_name TEXT, -- not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE reply (
+    reply_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_user_id INTEGER, -- fk
+    to_user_id INTEGER, -- fk
+    post_id INTEGER, -- fk
+    message TEXT, -- not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE review (
+    review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_user_id INTEGER, -- fk
+    to_user_id INTEGER, -- fk
+    content TEXT, -- not null
+    score INTEGER, -- not null
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
