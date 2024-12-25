ceba3f2b8fa68ed453c9e1ac100cd58624b5e973
JLin <jlin890516@gmail.com>
2024-12-25 08:42:22 +0800

    feat:add register file

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="modal-overlay" @click.self="emits('close')">
+    <div class="modal-content">
+      <h3 class="modal-title">註冊</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
