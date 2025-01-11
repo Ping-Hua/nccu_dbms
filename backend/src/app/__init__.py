a0bfccded43a61d4b8d73968e933cc03918e114e
JLin <jlin890516@gmail.com>
2025-01-11 19:19:50 +0800

    fix: remove unused file'

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -13,7 +13,6 @@ from app.middlewares.cors import cors_middleware
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # cors_middleware(app)
     # CORS(app , supports_credentials=True, origins=["http://localho.st:5173"])   
     CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://127.0.0.1:5173"}})
 
