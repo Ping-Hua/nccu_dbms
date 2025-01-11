0b9eec104440903e96e0ebca74b03be67ff2a90a
shiloh <shilohlee98@gmail.com>
2025-01-11 18:08:14 +0800

    Remove book version from book table

--- backend/src/app/services/book_service.py
+++ backend/src/app/services/book_service.py
@@ -5,11 +5,11 @@ from app.errors.custom_exceptions import ResourceNotFoundError, ValidationError
 class BookService:
     @staticmethod
     @use_db
-    def adding_book(cursor, ISBN, book_name, author, version, public_year, publisher, book_picture_url, genre_id):
-        logging.info('info', 'adding book called with parameter: ISBN={ISBN}, book_name={book_name}, author={author}, version={version}, public_year={public_year}, publisher={publisher}')
+    def adding_book(cursor, ISBN, book_name, author, public_year, publisher, book_picture_url, genre_id):
+        logging.info('info', 'adding book called with parameter: ISBN={ISBN}, book_name={book_name}, author={author}, public_year={public_year}, publisher={publisher}')
         
-        if any(v is None for v in [ISBN, book_name, author, version, public_year, publisher, book_picture_url, genre_id]):
-            raise ValidationError("Missing required fields: ISBN, book_name, author, version, public_year, publisher, book_picture_url, genre_id")
+        if any(v is None for v in [ISBN, book_name, author, public_year, publisher, book_picture_url, genre_id]):
+            raise ValidationError("Missing required fields: ISBN, book_name, author, public_year, publisher, book_picture_url, genre_id")
 
         # 判斷 ISBN 唯一
         cursor.execute(
@@ -20,8 +20,8 @@ class BookService:
             raise ValidationError(f"ISBN: {ISBN} already exists in the database.")
 
         cursor.execute(
-            "INSERT INTO book (ISBN, book_name, author, version, public_year, publisher, book_picture_url, genre_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
-            (ISBN, book_name, author, version, public_year, publisher, book_picture_url, genre_id)
+            "INSERT INTO book (ISBN, book_name, author, public_year, publisher, book_picture_url, genre_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+            (ISBN, book_name, author, public_year, publisher, book_picture_url, genre_id)
         )
 
         book_id = cursor.lastrowid # 獲取新增書籍的 ID
@@ -32,7 +32,6 @@ class BookService:
             "book_name": book_name,
             "ISBN": ISBN,      
             "author": author,
-            "version": version,
             "public_year": public_year,
             "publisher": publisher,
             "book_picture_url": book_picture_url,
@@ -45,7 +44,7 @@ class BookService:
     @use_db
     def get_all_books(cursor):
         cursor.execute(
-            "SELECT book_id, book_name, ISBN, author, version, public_year, book_picture_url, genre_id FROM book"
+            "SELECT book_id, book_name, ISBN, author, public_year, book_picture_url, genre_id FROM book"
         )
         books = cursor.fetchall()
         if books is None:
@@ -57,10 +56,9 @@ class BookService:
                 "book_name": book[1],
                 "ISBN": book[2],
                 "author": book[3],
-                "version": book[4],
-                "public_year": book[5],
-                "book_picture_url": book[6],
-                "genre_id": book[7]
+                "public_year": book[4],
+                "book_picture_url": book[5],
+                "genre_id": book[6]
             }
             book_json.append(book_data)
         return book_json
@@ -69,7 +67,7 @@ class BookService:
     @use_db
     def get_all_books_by_genre(cursor, genre_id):
         cursor.execute(
-            "SELECT book_id, book_name, ISBN, author, version, public_year, book_picture_url, genre_id FROM book "
+            "SELECT book_id, book_name, ISBN, author, public_year, book_picture_url, genre_id FROM book "
             "WHERE genre_id = ?",
             (genre_id,)
         )
@@ -83,10 +81,9 @@ class BookService:
                 "book_name": book[1],
                 "ISBN": book[2],
                 "author": book[3],
-                "version": book[4],
-                "public_year": book[5],
-                "book_picture_url": book[6],
-                "genre_id": book[7]
+                "public_year": book[4],
+                "book_picture_url": book[5],
+                "genre_id": book[6]
             }
             book_json.append(book_data)
         return book_json
